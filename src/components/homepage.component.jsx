40d80ae6ac9c44dc5dfe45c21ce6a250c0d4d28e
Tiago Belchior <tiagobelchior@Tiagos-MacBook-Pro.local>
2020-01-04 16:55:47 +0100

    created project and add first files

---
+++ src/components/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/homepage.styles.scss'
+
+export const HomePage = (props) => { return (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h2 className="title">HATS</h2>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)};
