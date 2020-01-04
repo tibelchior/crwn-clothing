72f7ee41c03c00c5ce0ee43b0961fd5a1cf98f9a
Tiago Belchior <tiagobelchior@Tiagos-MacBook-Pro.local>
2020-01-04 19:01:12 +0100

    start router

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 import './menu-item.styles.scss'
 
-const MenuItem = ({title,imageUrl,size}) => {
+const MenuItem = ({title,imageUrl,size,history,linkUrl,match}) => {
   size = size ? size : '';
   return (
-  <div className={`menu-item ${size}`}>
+  <div className={`menu-item ${size}`} onClick={()=>{history.push(`${match.url}${linkUrl}`)}}>
     <div className="background-image" style={{
     backgroundImage: `url(${imageUrl})`
   }} ></div>
@@ -15,4 +16,4 @@ const MenuItem = ({title,imageUrl,size}) => {
   </div>
 )};
 
-export default MenuItem;
+export default withRouter(MenuItem);
